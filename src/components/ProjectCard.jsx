8447961794e212c03a896660df71fe8f00257eeb
Maddy Capone <maddycapone@gmail.com>
2024-09-12 13:42:42 -0400

    add react carosel library to create project section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,11 @@
 
 const ProjectCard = ({title, img, alt}) => {
     return (
-        <section className="mt-6 mb-12 rounded-md h-32 w-1/5 hover:bg-accent transition duration-700 ease-in-out shadow-card flex flex-col justify-center items-center">
-            <h3 className="text-white-100">
+        <section className="mt-6 mb-12 rounded-md h-auto w-1/5 hover:bg-accent transition duration-700 ease-in-out shadow-card flex flex-col justify-center items-center">
+            <h3 className="text-white-100 text-sm px-4 py-4">
                 {title}
             </h3>
-            <img src={img} alt={alt} className="w-1/2"></img>
+            <img src={img} alt={alt} className="w-1/2 pb-4"></img>
         </section>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,40 @@
-
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const Projects = () => {
+
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 1,
+          slidesToSlide: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 1,
+          slidesToSlide: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1
+        }
+      };
+
     return (
-        <section  className="flex flex-col mx-6 md:mx-8 lg:mx-16 xl:mx-24">
+        <section className="flex flex-col mx-6 md:mx-8 lg:mx-16 xl:mx-24">
             <h2 className="text-3xl text-secondAccent py-2">Projects</h2>
-            <div className='flex flex-row justify-center gap-4'>
-            </div>
+            <Carousel 
+            responsive={responsive}
+            showDots={true}
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            keyBoardControl={true}
+            itemClass="carousel-item-padding-100-px">
+                <div>Item 1</div>
+                <div>Item 2</div>
+                <div>Item 3</div>
+                <div>Item 4</div>
+            </Carousel>;
         </section>
     );
 }
